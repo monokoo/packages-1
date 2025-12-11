ff744e0e9842a8a7177ca17d434171e8e5d8117d
monokoo <monokoo@users.noreply.github.com>
2025-12-11 10:43:52 +0800

    Enhance DNS record selection for Aliyun update scripts: support multiple records
    Refactor DNS record selection logic to handle specific RecordId if provided, otherwise defaults to the first record.

--- net/ddns-scripts/files/usr/lib/ddns/update_aliyun_com.sh
+++ net/ddns-scripts/files/usr/lib/ddns/update_aliyun_com.sh
@@ -113,10 +113,37 @@ json_get_var __RECORD_COUNT TotalCount
 # select the first DNS record
 json_select DomainRecords
 json_select Record
-json_select 1
-# get the record id of the first DNS record
-json_get_var __RECORD_ID RecordId
-json_get_var __RECORD_VALUE Value
+
+write_log 7 "param_enc: `echo ${param_enc}`"             
+paramEnc=${param_enc}                                    
+if [ -n "$(echo "${paramEnc}" | grep RecordId)"  ]; then 
+        specRecordId=$(echo "$paramEnc" | grep -o 'RecordId=[^&]*' | cut -d'=' -f2)
+        write_log 7 "specRecordId: ${specRecordId}"                                
+        idx=1                                                                      
+        while json_is_a $idx object                                                
+        do                                                                         
+            json_select $idx                                                       
+            json_get_var tmp RecordId                                              
+            write_log 7 "The $idx Domain RecordId: ${tmp}"                         
+            [ "$tmp" = "$specRecordId" ] && {                                      
+                __RECORD_ID=$specRecordId                                          
+                json_get_var __RECORD_VALUE Value                                  
+                write_log 7 "The $idx Domain Record Value: ${__RECORD_VALUE}"                                                      
+                break                                                              
+            }                                                                
+            idx=$((idx+1))                                                   
+            json_select ..                                                                    
+        done                                                                              
+else                                                                         
+    # if multiple records are found, only use the first one                  
+    [ "$__RECORD_COUNT" -gt 1 ] && {                                                                             
+            write_log 4 "WARNING: found multiple records of $__HOST, only use the first one"
+    }                                                                                       
+    json_select 1                                                                           
+    # get the record id of the first DNS record                                             
+    json_get_var __RECORD_ID RecordId                                                       
+    json_get_var __RECORD_VALUE Value                                                       
+fi
 
 # dont update if the ip has not changed
 [ "$__RECORD_VALUE" = "$__IP" ] && {
